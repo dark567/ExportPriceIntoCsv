772f68d6586bd47f0428f4368d646eb772dcce3b
Сергей Митяков <sergey@astarta.ua>
2020-01-17 17:14:14 +0200

    20200117_02

--- WindowsFormsApplication1/MainForm.cs
+++ WindowsFormsApplication1/MainForm.cs
@@ -28,7 +28,7 @@ namespace ExportPriceFor1C
 
             string date = crypto.GetDecodeKey("keyfile.dat").Substring(crypto.GetDecodeKey("keyfile.dat").IndexOf("|") + 1);
 
-            if (DateTime.Parse(date) <= DateTime.Now) Close();
+            if (DateTime.Parse(date).AddDays(1) <= DateTime.Now) Close();
             this.Text = this.Text + "......." + date;
         }
 
